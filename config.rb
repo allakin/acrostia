96cfa3f1767e4ef43929bbf7e4a99f0f6608c05c
Pavel <19paul91@mail.ru>
2014-10-07 13:32:45 +0400

    add menu

---
+++ config.rb
@@ -0,0 +1,17 @@
+#папка куда складывать css
+css_dir = "css"
+#папка откуда брать scss
+sass_dir = "scss"
+#папка откуда брать картинки
+images_dir = "img"
+#папка откуда брать спрайты
+#sprite_load_path = "img/sprite"
+#уровень компрессии css файла
+#:nested
+#:expanded
+#:compact
+#:compressed
+output_style = :expanded
+#отображать комментарии
+line_comments = false
+relative_assets = true
---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+        mode: 'fade',
+        nextSelector: '#slider-next',
+        prevSelector: '#slider-prev',
+        nextText: '',
+        pager: false,
+        prevText: ''
+    });
+
+    $('.header-slider').bxSlider({
+        mode: 'fade',
+        nextSelector: '#header-slider-next',
+        prevSelector: '#header-slider-prev',
+        nextText: '',
+        pager: false,
+        //auto: true,
+        prevText: ''
+    });
+});
